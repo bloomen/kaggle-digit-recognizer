feb2714e272f2dc5d75e748c7e67078695f3fb41
Christian Blume <chr.blume@gmail.com>
2020-03-28 05:25:47 +1300

    use alexnet

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
+import os
+import argparse
+import pickle
 import pylab as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-import os
 from sklearn.base import TransformerMixin, BaseEstimator
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import label
-from sklearn.linear_model.logistic import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_validate, train_test_split, GridSearchCV
-from sklearn.svm import SVC, LinearSVC, SVR
-from sklearn.preprocessing.data import MinMaxScaler, StandardScaler, RobustScaler
-from sklearn.preprocessing.data import PolynomialFeatures
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.decomposition import PCA
-from sklearn import preprocessing
 from sklearn.metrics import accuracy_score, roc_curve, auc
 from sklearn.utils import assert_all_finite
-from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.neural_network import MLPClassifier
-from featureimpact import FeatureImpact, averaged_impact
-import argparse
-import pickle
-import tensorflow as tf
+from sklearn.ensemble import VotingClassifier
 from sklearn.utils import shuffle
+from sklearn.pipeline import Pipeline
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 
 np.random.seed(42)
-
 
 IMAGE_SIZE = 28
 NUM_LABELS = 10
@@ -39,116 +27,106 @@ class Reshaper(TransformerMixin):
 
     def transform(self, X, *_):
         X = X.reshape((-1, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNGELS))
-        X = np.pad(X, ((0, 0),(2, 2),(2, 2),(0, 0)), 'constant')
         return X
 
     def fit(self, *_):
         return self
 
 
-class LeNet(BaseEstimator):
+class AlexNet(BaseEstimator):
 
-    EPOCHS = 10
-    BATCH_SIZE = 128
-    PATH = 'output/lenet.ckpt'
-
-    def __init__(self):
-        self._X = tf.compat.v1.placeholder(tf.float32, shape=[None, 32, 32, 1])
-        self._y = tf.compat.v1.placeholder(tf.int32)
-        #Invoke LeNet function by passing features
-        self._logits = le_net_5(self._X)
-        #Softmax with cost function implementation
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self._y, logits=self._logits)
-        loss_operation = tf.reduce_mean(cross_entropy)
-        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001)
-        self._training_operation = optimizer.minimize(loss_operation)
-        correct_prediction = tf.equal(tf.argmax(self._logits, 1), tf.argmax(self._y, 1))
-        self._accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    def __init__(self, index):
+        self._path = 'output/alexnet_{}.h5'.format(index)
 
     def fit(self, X, y):
+        model = self._create_model()
         y = pd.get_dummies(y).values
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
-        with tf.compat.v1.Session() as sess:
-            sess.run(tf.compat.v1.global_variables_initializer())
-            num_examples = len(X_train)
-            print("Training... with dataset - ", num_examples)
-            print()
-            for i in range(self.EPOCHS):
-                X_train, y_train = shuffle(X_train, y_train)
-                for offset in range(0, num_examples, self.BATCH_SIZE):
-                    end = offset + self.BATCH_SIZE
-                    batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-                    sess.run(self._training_operation, feed_dict={self._X: batch_x, self._y: batch_y})
-                validation_accuracy = self._evaluate(sess, X_val, y_val)
-                print("EPOCH {} ...".format(i+1))
-                print("Validation Accuracy = {:.3f}".format(validation_accuracy))
-                print()
-            saver = tf.compat.v1.train.Saver()
-            saver.save(sess, self.PATH)
-            print("Model saved {}".format(self.PATH))
+        model.fit(X, y, batch_size=64, epochs=3, verbose=1, validation_split=0.1, shuffle=True)
+        model.save(self._path)
 
     def predict(self, X):
-        with tf.compat.v1.Session() as sess:
-            saver = tf.compat.v1.train.Saver()
-            saver.restore(sess, self.PATH)
-            print("Model restored {}".format(self.PATH))
-            Z = self._logits.eval(feed_dict={self._X: X})
-            return np.argmax(Z, axis=1)
- 
-    def _evaluate(self, sess, X_data, y_data):
-        num_examples = len(X_data)
-        total_accuracy = 0
-        for offset in range(0, num_examples, self.BATCH_SIZE):
-            batch_x, batch_y = X_data[offset:offset+self.BATCH_SIZE], y_data[offset:offset+self.BATCH_SIZE]
-            accuracy = sess.run(self._accuracy_operation, feed_dict={self._X: batch_x, self._y: batch_y})
-            total_accuracy += (accuracy * len(batch_x))
-        return total_accuracy / num_examples
+        model = keras.models.load_model(self._path)
+        y = model.predict(X)
+        return np.argmax(y, axis=1)
 
+    def _create_model(self):
+        # (3) Create a sequential model
+        model = Sequential()
 
-# LeNet-5 architecture implementation using TensorFlow
-def le_net_5(x):
-     # Layer 1 : Convolutional Layer. Input = 32x32x1, Output = 28x28x1.
-     conv1_w = tf.Variable(tf.random.truncated_normal(shape = [5,5,1,6],mean = 0, stddev = 0.1))
-     conv1_b = tf.Variable(tf.zeros(6))
-     conv1 = tf.nn.conv2d(x,conv1_w, strides = [1,1,1,1], padding = 'VALID') + conv1_b 
-     # TODO: Activation.
-     conv1 = tf.nn.relu(conv1)
-     
-     # Pooling Layer. Input = 28x28x1. Output = 14x14x6.
-     pool_1 = tf.nn.max_pool2d(conv1, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
-     
-     # TODO: Layer 2: Convolutional. Output = 10x10x16.
-     conv2_w = tf.Variable(tf.random.truncated_normal(shape = [5,5,6,16], mean = 0, stddev = 0.1))
-     conv2_b = tf.Variable(tf.zeros(16))
-     conv2 = tf.nn.conv2d(pool_1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b
-     # TODO: Activation.
-     conv2 = tf.nn.relu(conv2)
-    # TODO: Pooling. Input = 10x10x16. Output = 5x5x16.
-     pool_2 = tf.nn.max_pool2d(conv2, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
-     
-     # TODO: Flatten. Input = 5x5x16. Output = 400.
-     fc1 = tf.compat.v1.layers.flatten(pool_2)
-     
-     # TODO: Layer 3: Fully Connected. Input = 400. Output = 120.
-     fc1_w = tf.Variable(tf.random.truncated_normal(shape = (400,120), mean = 0, stddev = 0.1))
-     fc1_b = tf.Variable(tf.zeros(120))
-     fc1 = tf.matmul(fc1,fc1_w) + fc1_b
-     
-     # TODO: Activation.
-     fc1 = tf.nn.relu(fc1)
-     
-     # TODO: Layer 4: Fully Connected. Input = 120. Output = 84.
-     fc2_w = tf.Variable(tf.random.truncated_normal(shape = (120,84), mean = 0, stddev = 0.1))
-     fc2_b = tf.Variable(tf.zeros(84))
-     fc2 = tf.matmul(fc1,fc2_w) + fc2_b
-     # TODO: Activation.
-     fc2 = tf.nn.relu(fc2)
-     
-     # TODO: Layer 5: Fully Connected. Input = 84. Output = 10.
-     fc3_w = tf.Variable(tf.random.truncated_normal(shape = (84,10), mean = 0 , stddev = 0.1))
-     fc3_b = tf.Variable(tf.zeros(10))
-     logits = tf.matmul(fc2, fc3_w) + fc3_b
-     return logits
+        # 1st Convolutional Layer
+        model.add(Conv2D(filters=96, input_shape=(28,28,1), kernel_size=(3,3),
+                         strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Pooling 
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # Batch Normalisation before passing it to the next layer
+        model.add(BatchNormalization())
+
+        # 2nd Convolutional Layer
+        model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Pooling
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # 3rd Convolutional Layer
+        model.add(Conv2D(filters=384, kernel_size=(2,2), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # 4th Convolutional Layer
+        model.add(Conv2D(filters=384, kernel_size=(2,2), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # 5th Convolutional Layer
+        model.add(Conv2D(filters=256, kernel_size=(2,2), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Pooling
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # Passing it to a dense layer
+        model.add(Flatten())
+        # 1st Dense Layer
+        model.add(Dense(4096, input_shape=(256,)))
+        model.add(Activation('relu'))
+        # Add Dropout to prevent overfitting
+        model.add(Dropout(0.4))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # 2nd Dense Layer
+        model.add(Dense(4096))
+        model.add(Activation('relu'))
+        # Add Dropout
+        model.add(Dropout(0.4))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # 3rd Dense Layer
+        model.add(Dense(1000))
+        model.add(Activation('relu'))
+        # Add Dropout
+        model.add(Dropout(0.4))
+        # Batch Normalisation
+        model.add(BatchNormalization())
+
+        # Output Layer
+        model.add(Dense(NUM_LABELS))
+        model.add(Activation('softmax'))
+
+        model.summary()
+
+        # (4) Compile 
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        return model
 
 
 class CopyTransformer(TransformerMixin):
@@ -171,12 +149,12 @@ def train_data():
 def test_data():
     df = pd.read_csv('input/test.csv', dtype=np.float32)
     return df.values
-    
-    
+
+
 def make_model():
     return Pipeline([
         ('reshaper', Reshaper()),
-        ('model', LeNet()),
+        ('model', VotingClassifier([("model_{}".format(i), AlexNet(i)) for i in range(20)])),
     ])
 
 
@@ -212,11 +190,6 @@ def main():
     if args.train == 'cv':
         X, y = train_data()
         model = make_model()
-        X_train, X_test, y_train, y_test = train_test_split(X, y)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        print(y_pred.shape, y_test.shape)
-        print("Accuracy:", accuracy_score(y_test, y_pred))
         print("Full training...")
         model.fit(X, y)
         return
